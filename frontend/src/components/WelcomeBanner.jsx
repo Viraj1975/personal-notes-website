bcba4bf30d4481cd20657e9d7f2ea32846a93d3e
viraj1975 <virajrajput1975@gmail.com>
2025-04-11 16:27:01 +0530

    adding Welocome Banner.jsx

---
+++ frontend/src/components/WelcomeBanner.jsx
@@ -0,0 +1,11 @@
+function WelcomeBanner(){
+    return (
+        <div className="bg-blue-100 border-l-4 border-blue-500 p-4 mb-6 rounded">
+            <h2 className="text-blue-700 font-semibold text-lg">Welcome to Personal Notes 📒</h2>
+            <p className="text-sm text-blue-600">
+            Quickly jot down and organize your thoughts, ideas, and lists!
+            </p>
+        </div>
+    );
+}
+export default WelcomeBanner;
